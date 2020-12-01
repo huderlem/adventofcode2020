4832b62875835b14145b6ba21a83622e5d9f9bf5
Marcus Huderle <huderlem@gmail.com>
2020-11-30 19:08:16 -0600

    Setup skeleton for solutions and some file utility functions

---
+++ day1/main.go
@@ -0,0 +1,21 @@
+// Solution for Advent of Code 2020 -- Day 1
+// https://adventofcode.com/2020/day/1
+
+package main
+
+import (
+	"fmt"
+)
+
+func part1() int {
+	return 0
+}
+
+func part2() int {
+	return 0
+}
+
+func main() {
+	fmt.Println("Part 1 Answer:", part1())
+	fmt.Println("Part 2 Answer:", part2())
+}
