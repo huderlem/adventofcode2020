634c95e6aaf20d5d72b79157ba775edbc45fdc22
Marcus Huderle <huderlem@gmail.com>
2020-12-03 15:55:56 -0600

    Day 1 solution

--- day1/main.go
+++ day1/main.go
@@ -5,14 +5,38 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/huderlem/adventofcode2020/util"
 )
 
 func part1() int {
-	return 0
+	expenses := util.ReadFileInts("input.txt")
+	for i := 0; i < len(expenses)-2; i++ {
+		for j := i + 1; j < len(expenses)-1; j++ {
+			sum := expenses[i] + expenses[j]
+			if sum == 2020 {
+				return expenses[i] * expenses[j]
+			}
+		}
+	}
+	// Valid expense combination not found.
+	return -1
 }
 
 func part2() int {
-	return 0
+	expenses := util.ReadFileInts("input.txt")
+	for i := 0; i < len(expenses)-2; i++ {
+		for j := i + 1; j < len(expenses)-1; j++ {
+			for k := j + 1; k < len(expenses); k++ {
+				sum := expenses[i] + expenses[j] + expenses[k]
+				if sum == 2020 {
+					return expenses[i] * expenses[j] * expenses[k]
+				}
+			}
+		}
+	}
+	// Valid expense combination not found.
+	return -1
 }
 
 func main() {
